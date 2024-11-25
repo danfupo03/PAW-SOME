4c63a31792dc8bed09818efeb67cc017e4b63a27
danfupo03 <danfupo@outlook.com>
2024-11-25 20:20:13 +0100

    feat(customer): add color validation in customer form

---
+++ assets/js/customer.js
@@ -0,0 +1,45 @@
+function changeBorderColor(inputElement, isValid, errorElement) {
+  if (isValid) {
+    inputElement.style.borderColor = "green";
+    errorElement.style.display = "none";
+  } else {
+    inputElement.style.borderColor = "red";
+    errorElement.style.display = "block";
+  }
+}
+
+username = document.getElementById("username");
+password = document.getElementById("password");
+
+username.addEventListener("input", function () {
+  let isValidUsername =
+    /^(?=.*[a-z])(?=.*[A-Z])/.test(username.value) &&
+    username.value.length >= 5;
+  changeBorderColor(
+    username,
+    isValidUsername,
+    document.getElementById("usernameError")
+  );
+});
+
+password.addEventListener("input", function () {
+  changeBorderColor(
+    password,
+    password.value.length >= 10,
+    document.getElementById("passwordError")
+  );
+});
+
+document
+  .getElementById("customerForm")
+  .addEventListener("submit", function (event) {
+    let isValidUsername =
+      username.value.length >= 5 &&
+      /^(?=.*[a-z])(?=.*[A-Z])/.test(username.value);
+    let isValidPassword = password.value.length >= 10;
+
+    if (!isValidUsername || !isValidPassword) {
+      event.preventDefault();
+      alert("Form is not valid");
+    }
+  });
