82dfda5db27bde0935a0e14d6b14d5f963d306a7
danfupo03 <danfupo@outlook.com>
2024-11-27 14:58:45 +0100

    chore(dark-mode): fix bug with dark mode

--- assets/js/register.js
+++ assets/js/register.js
@@ -39,17 +39,39 @@ confirmPassword.addEventListener("input", function () {
   );
 });
 
-document.getElementById("registerForm").addEventListener("submit", function (event) {
-  let isValidUsername =
-    username.value.length >= 5 &&
-    /^(?=.*[a-z])(?=.*[A-Z])/.test(username.value);
-  let isValidPassword = password.value.length >= 10;
-  let isValidConfirmPassword = password.value === confirmPassword.value;
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", function (event) {
+    let isValidUsername =
+      username.value.length >= 5 &&
+      /^(?=.*[a-z])(?=.*[A-Z])/.test(username.value);
+    let isValidPassword = password.value.length >= 10;
+    let isValidConfirmPassword = password.value === confirmPassword.value;
 
-  if (!isValidUsername || !isValidPassword || !isValidConfirmPassword) {
-    event.preventDefault();
-    alert("Form is not valid");
+    if (!isValidUsername || !isValidPassword || !isValidConfirmPassword) {
+      event.preventDefault();
+      alert("Form is not valid");
+    } else {
+      alert("Account created successfully");
+    }
+  });
+
+function darkMode() {
+  let body = document.body;
+  let button = document.getElementById("dark-mode");
+  let icon = document.querySelector("#dark-mode i");
+
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+    button.classList.remove("is-dark");
+    button.classList.add("is-light");
   } else {
-    alert("Account created successfully");
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+    button.classList.remove("is-light");
+    button.classList.add("is-dark");
   }
-});
+}
