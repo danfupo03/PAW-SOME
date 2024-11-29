0989dce41b8908a1c1d0a5c173dda09bfa11e680
danfupo03 <danfupo@outlook.com>
2024-11-29 15:45:46 +0100

    feat(includes): add includes to files

--- views/login.php
+++ views/login.php
@@ -1,21 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Paw-Some &#128054;</title>
-  <link rel="icon" href="/assets/images/favicon.ico" />
-
-  <!-- Font Awesome -->
-  <script
-    src="https://kit.fontawesome.com/87b5cb0a2b.js"
-    crossorigin="anonymous"></script>
-
-  <!-- Stylesheet -->
-  <link defer rel="stylesheet" href="../assets/css/styles.css" />
-  <link defer rel="stylesheet" href="../assets/css/variables.css" />
-</head>
+<?php
+include 'includes/head.php';
+?>
 
 <body class="body">
   <div class="mt-3">
