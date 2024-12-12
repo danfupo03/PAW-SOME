81d2aa592d95acee9a680371de8c042379947855
danfupo03 <danfupo@outlook.com>
2024-12-12 12:01:53 +0100

    feat(DarkMode): add persistent darkmode

--- assets/js/about.js
+++ assets/js/about.js
@@ -46,23 +46,3 @@ languageSelector.addEventListener("change", function () {
   document.querySelectorAll("h2.title.is-3")[2].textContent =
     translations[selectedLanguage].contactUs;
 });
-
-function darkMode() {
-  let body = document.body;
-  let button = document.getElementById("dark-mode");
-  let icon = document.querySelector("#dark-mode i");
-
-  body.classList.toggle("dark-mode");
-
-  if (body.classList.contains("dark-mode")) {
-    icon.classList.remove("fa-moon");
-    icon.classList.add("fa-sun");
-    button.classList.remove("is-dark");
-    button.classList.add("is-light");
-  } else {
-    icon.classList.remove("fa-sun");
-    icon.classList.add("fa-moon");
-    button.classList.remove("is-light");
-    button.classList.add("is-dark");
-  }
-}
